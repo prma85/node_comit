1028befd0cc44fcefef2214982276ff873e0c55e
Paulo Andrade <pauloa@iqmetrix.com>
2020-04-14 19:13:22 -0600

    first part of superheros in react

---
+++ resources/solution/react/superheroes/src/layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container} from 'react-bootstrap';
+import Main from './main';
+
+const Layout = () => {
+  return (
+    <Container>
+      <Main />
+    </Container>
+  )
+}
+
+export default Layout;
---
+++ resources/solution/react/superheroes/src/main.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Row, Col, Card, Button } from "react-bootstrap";
+import Superheroes from "./db/superheroes";
+
+const Main = () => {
+  const superheroesWrapper = () => {
+    return Superheroes.map((superhero, index) => {
+      return (
+        <Card className="superhero" style={{ width: "30%" }} key={index}>
+          <Card.Img variant="top" src={"img/superheroes/" + superhero.image} />
+          <Card.Title>{superhero.name}</Card.Title>
+          <Button href="" variant="primary">Show me more!</Button>
+        </Card>
+      );
+    });
+  };
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Wellcome to React</h1>
+          <p>This project is just to practice</p>
+        </Col>
+      </Row>
+      <Row>{superheroesWrapper()}</Row>
+    </>
+  );
+};
+
+export default Main;
